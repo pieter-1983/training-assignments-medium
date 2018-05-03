b76ad8d25c623bf9dc8ae2e1d5874d53af5ca9e1
pieter-1983 <pieter.sterckx@cegeka.com>
2018-05-03 15:19:54 +0200

    huphup

---
+++ src/main/java/com/netflix/simianarmy/aws/DatabaseConnectionConfig.java
@@ -0,0 +1,39 @@
+package com.netflix.simianarmy.aws.janitor;
+
+public class DatabaseConnectionConfig {
+    private final String dbDriver;
+    private final String dbPass;
+    private final String dbUser;
+    private final String dbUrl;
+    private final String dbTable;
+
+    /**
+     */
+    public DatabaseConnectionConfig(String dbDriver, String dbPass, String dbUser, String dbUrl, String dbTable) {
+        this.dbDriver = dbDriver;
+        this.dbPass = dbPass;
+        this.dbUser = dbUser;
+        this.dbUrl = dbUrl;
+        this.dbTable = dbTable;
+    }
+
+    public String getDbDriver() {
+        return dbDriver;
+    }
+
+    public String getDbPass() {
+        return dbPass;
+    }
+
+    public String getDbUser() {
+        return dbUser;
+    }
+
+    public String getDbUrl() {
+        return dbUrl;
+    }
+
+    public String getDbTable() {
+        return dbTable;
+    }
+}
---
+++ src/main/java/com/netflix/simianarmy/conformity/SoloCluster.java
@@ -0,0 +1,4 @@
+package com.netflix.simianarmy.conformity;
+
+public class SoloCluster {
+}
